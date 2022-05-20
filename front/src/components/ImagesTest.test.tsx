d00d995f417c266faa89c7e89be32bfb701a0d0a
Konrad Dziekonski <jaseveen@gmail.com>
2022-05-20 22:38:20 +0200

    feat(sample files): add sample files to test ci

---
+++ front/src/components/ImagesTest.test.tsx
@@ -0,0 +1,12 @@
+import { render, screen } from "@testing-library/react";
+import { ImagesTest } from "./ImagesTest";
+
+// describe("when rendered with a `name` prop", () => {
+//   it("should paste it into the greetings text", () => {
+//     render(<ImagesTest props="Test Name" />);
+//     expect(screen.getByText(/Hello Test Name!/)).toBeDefined();
+//   });
+// });
+it("runs the first test", () => {
+  expect(true).toBe(true);
+});
--- front/src/components/ImagesTest.tsx
+++ front/src/components/ImagesTest.tsx
@@ -1,17 +1,17 @@
-import img1 from "../assets/confrontjs-banner.png";
-import img2 from "../assets/cart.svg";
-import img3 from "../assets/work-life-balance.gif";
+import Img2 from "../assets/cart.svg";
+import Img1 from "../assets/confrontjs-banner.png";
+import Img3 from "../assets/work-life-balance.gif";
 
 interface P {
-  props: string;
+  readonly props: string;
 }
 export const ImagesTest = ({ props }: P) => {
   return (
     <div>
-      <span>{props}</span>
-      <img src={img1} className="h-96" />
-      <img src={img2} className="h-96" />
-      <img src={img3} className="h-96" />
+      <span className="text-5xl font-light">{props}</span>
+      <img src={Img1} className="h-96" />
+      <img src={Img2} className="h-96" />
+      <img src={Img3} className="h-96" />
     </div>
   );
 };
