3f4e2a62ae6652d2892a18d1e7f1959e7d5184c1
Konrad Dziekonski <jaseveen@gmail.com>
2022-05-22 15:15:02 +0200

    feat(backend): add inital backend markup

---
+++ backend/dist/index.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
+var app = express();
+var port = process.env.PORT;
+app.get('/', function (req, res) {
+    res.send('Express + TypeScript Server');
+});
+app.listen(port, function () {
+    console.log("\u26A1\uFE0F[server]: Server is running at https://localhost:".concat(port));
+});
+//# sourceMappingURL=index.js.map
